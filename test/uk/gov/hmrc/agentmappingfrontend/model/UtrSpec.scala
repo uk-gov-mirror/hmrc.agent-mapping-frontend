eaaec44a1ba824d8c710f2cc6e2f2418e5f70663
ralphcollett <ralph.collett@gmail.com>
2017-04-20 10:38:44 +0100

    [RC][APB-704] refactor test to make name read better.

--- test/uk/gov/hmrc/agentmappingfrontend/model/UtrSpec.scala
+++ test/uk/gov/hmrc/agentmappingfrontend/model/UtrSpec.scala
@@ -24,24 +24,26 @@ class UtrSpec extends UnitSpec {
       Utr.isValid("2000000000") shouldBe true
     }
 
-    "it has more than 10 digits" in {
-      Utr.isValid("20000000000") shouldBe false
-    }
+    "be false" when {
+      "it has more than 10 digits" in {
+        Utr.isValid("20000000000") shouldBe false
+      }
 
-    "it has fewer than 10 digits" in {
-      Utr.isValid("200000") shouldBe false
-    }
+      "it has fewer than 10 digits" in {
+        Utr.isValid("200000") shouldBe false
+      }
 
-    "it has non-digit characters" in {
-      Utr.isValid("200000000B") shouldBe false
-    }
+      "it has non-digit characters" in {
+        Utr.isValid("200000000B") shouldBe false
+      }
 
-    "it has non-alphanumeric characters" in {
-      Utr.isValid("200000000!") shouldBe false
-    }
+      "it has non-alphanumeric characters" in {
+        Utr.isValid("200000000!") shouldBe false
+      }
 
-    "be false when the checksum doesn't pass" in {
-      Utr.isValid("2000000001") shouldBe false
+      "be false when the checksum doesn't pass" in {
+        Utr.isValid("2000000001") shouldBe false
+      }
     }
   }
 }
