e99a10b105619dbc57f9b7847f1438072bd12691
Paul Hodgson <paul.hodgson@digital.hmrc.gov.uk>
2017-04-24 10:10:48 +0100

    [APB-703][ph,ta] complete page displays provided arn and saRef

---
+++ app/uk/gov/hmrc/agentmappingfrontend/controllers/UrlBinders.scala
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.agentmappingfrontend.controllers
+
+import uk.gov.hmrc.agentmappingfrontend.model.Arn
+import uk.gov.hmrc.domain.SaAgentReference
+import uk.gov.hmrc.play.binders.SimpleObjectBinder
+
+object UrlBinders {
+  implicit val arnBinder = new SimpleObjectBinder[Arn](Arn.apply, _.value)
+  implicit val saAgentReferenceBinder = new SimpleObjectBinder[SaAgentReference](SaAgentReference.apply, _.value)
+}
