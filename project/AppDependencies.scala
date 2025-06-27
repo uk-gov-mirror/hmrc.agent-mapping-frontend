eeb478a8fcae96295566130979eb3d7629a57c9f
Alistair Macdonald <1460971+nubz@users.noreply.github.com>
2025-06-27 15:10:04 +0100

    APB-9622 Govuk rebrand (#278)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,13 +3,13 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVer: String = "9.12.0"
+  private val bootstrapVer: String = "9.13.0"
   private val mongoVer: String = "2.6.0"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc"        %% "bootstrap-frontend-play-30" % bootstrapVer,
-    "uk.gov.hmrc"        %% "play-frontend-hmrc-play-30" % "12.1.0",
+    "uk.gov.hmrc"        %% "play-frontend-hmrc-play-30" % "12.6.0",
     "uk.gov.hmrc"        %% "play-partials-play-30"      % "10.0.0",
     "uk.gov.hmrc.mongo"  %% "hmrc-mongo-play-30"         % mongoVer,
     "uk.gov.hmrc"        %% "agent-mtd-identifiers"      % "2.2.0",
